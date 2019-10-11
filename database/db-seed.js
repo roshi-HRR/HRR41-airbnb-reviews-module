e429586a91fed5e8d5eb71796e1e4b5284fc3a33
RachNovo <r.novoselac@gmail.com>
2019-10-11 01:05:46 -0400

    State of completion at end of day

--- database/db-seed.js
+++ database/db-seed.js
@@ -1,3 +1,8 @@
 const Reviews = require('./model.js');
 const db = require('./db.js');
 
+//TODO: create functions
+//require faker.js
+//individualReview func (creates individual reviews)
+//totalReviewDoc func (creates total review with random # of individualReviews)
+//add to db func (adds the result of totalReviewDoc to db 100 times)
