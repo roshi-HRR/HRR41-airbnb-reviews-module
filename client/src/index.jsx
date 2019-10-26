00c40e82cb8cb813abe14baf2e622c7ebe82ec22
RachNovo <r.novoselac@gmail.com>
2019-10-26 17:39:17 -0400

    Changed the id used in ReactDOM.render to review.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -38,6 +38,6 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('review'));
 
 export default App;
