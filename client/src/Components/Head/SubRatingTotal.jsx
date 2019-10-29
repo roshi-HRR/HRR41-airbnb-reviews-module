a95303062c457171653608d17c8f46c5a1b9aec3
RachNovo <r.novoselac@gmail.com>
2019-10-28 22:02:34 -0400

    Refactored to add review bar animation.

--- client/src/Components/Head/SubRatingTotal.jsx
+++ client/src/Components/Head/SubRatingTotal.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import styles, {main, line, numbers, category, categoryContainer} from './SubRatingTotal.css';
+import styles, {main, line, numbers, category, categoryContainer, under} from './SubRatingTotal.css';
 
 class SubRatingTotal extends Component {
   constructor(props) {
@@ -7,10 +7,14 @@ class SubRatingTotal extends Component {
   }
   render() {
     var rating = this.props.rating;
+    console.log(rating.rating * 20);
     return (
       <div className={main}>
         <span className={category}>{rating.category}</span>
-        <span><img className={line} src='https://rachels-review-bucket.s3.amazonaws.com/Line.png' alt='*'></img></span>
+        <span>
+          <img className={line} width={rating.rating * 20} src='https://rachels-review-bucket.s3.amazonaws.com/Line2.png' alt='*'></img>
+          <img className={under} src='https://rachels-review-bucket.s3.amazonaws.com/Blank+Line.png'></img>
+          </span>
         <span className={numbers}>{rating.rating}</span>
       </div>
     )
